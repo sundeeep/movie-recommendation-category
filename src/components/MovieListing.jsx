169ddce1ae771d9b6e7343fd49f9ff7f569acba5
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-02-28 22:58:41 +0530

    some Tweaks and code cleaning

--- src/components/MovieListing.jsx
+++ src/components/MovieListing.jsx
@@ -10,11 +10,11 @@ function MovieListing({movies, isMoviesLoading}) {
     return(
         <div className='p-5 flex flex-wrap justify-center gap-5 items-center bg-[#120E43] -mt-[250px] '>
             {isMoviesLoading
-            ? skeletons.map((_, i) => <CardSkeletonUI key={i}/>)
-            :movies?.map(movie => (
-                <Link key={movie?.id} to={`/top-rated/${movie?.id}`}>
-                    <MovieCard poster={movie?.poster_path} name={movie?.original_title} date={movie?.release_date} popularity={movie?.popularity} votes={movie?.vote_count} votesAvg={movie?.vote_average}/>
-                </Link>
+                ? skeletons.map((_, i) => <CardSkeletonUI key={i}/>)
+                :movies?.map(movie => (
+                    <Link key={movie?.id} to={`/top-rated/${movie?.id}`}>
+                        <MovieCard poster={movie?.poster_path} name={movie?.original_title} date={movie?.release_date} popularity={movie?.popularity} votes={movie?.vote_count} votesAvg={movie?.vote_average}/>
+                    </Link>
             ))}
         </div>
     )
